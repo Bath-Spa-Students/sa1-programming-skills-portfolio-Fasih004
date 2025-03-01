ac609ea744f3073310af256876bebce846001c07
Fasih05 <fasihurrehman005@gmail.com>
2025-03-01 12:20:42 +0400

    Simple Sums

---
+++ Exercise2.py
@@ -0,0 +1,8 @@
+#Declare and initialize the first variable
+num1 = 8
+#Declare and initialize the second variable
+num2 = 10
+#Declare a third variable to store the sum
+sum_result = num1 + num2
+#Print the value of the sum to the console
+print("The sum is:", sum_result)
